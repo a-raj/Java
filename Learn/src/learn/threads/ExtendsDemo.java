3e1107b34c2a736956a7f9d2a3b8a2a9e91b404a
Avinash Raj <seriouswithavi@gmail.com>
2017-05-08 02:03:28 +0530

    Initial commit

---
+++ Learn/src/learn/threads/ExtendsDemo.java
@@ -0,0 +1,41 @@
+package learn.threads;
+
+
+/**
+ * What happens when you directly call run method for a thread?
+ * 
+ * Invoking the run method in the current thread, the method would goes into the current thread stack, 
+ * rather than the beginning of the new stack( which happens when you call the start method )
+ * */
+public class ExtendsDemo extends Thread {
+
+	
+	public void run(){
+		try{
+			for(int i = 1; i < 6; i++){
+				System.out.println("Inside for " + i + " Thread name " + this.getName());
+				Thread.sleep(1000);
+			}
+		}catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	public static void main(String[] args) {
+		ExtendsDemo extends1 = new ExtendsDemo();
+		ExtendsDemo extends12 = new 	ExtendsDemo();
+		
+		
+		 
+		
+		//Use either of the below to see what happens
+		extends1.start();
+		extends12.start();
+		
+		
+		/*
+		extends1.run();
+		extends12.run();
+		*/
+	}
+
+}
