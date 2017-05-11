0ceffa33f705d5804e82e8edca493906dee1d04d
Avinash Raj <seriouswithavi@gmail.com>
2017-05-12 01:35:31 +0530

    ThreadPool

--- Learn/src/threadPool/ThreadPoolTest.java
+++ Learn/src/threadPool/ThreadPoolTest.java
@@ -1,16 +1,8 @@
 package threadPool;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-/**
- * Class to test {@link TaskThread}<br>
- * It uses {@link ExecutorService} to create the thread pool<br>
- * In ThreadPoolTest, we can specify the number of task threads to create and the size of the thread pool that will be used to run the threads. 
- * This example uses a fixed thread pool so that we can observe the effect of running the program with fewer threads than tasks.
- * */
 public class ThreadPoolTest {
 
 	public static void main(String[] args) {
@@ -19,12 +11,11 @@ public class ThreadPoolTest {
 		
 		ExecutorService executorService = Executors.newFixedThreadPool(threadPoolSize);
 		
-		
-		List<TaskThread> tasks = new ArrayList<TaskThread>(numberOfTasks);
+		TaskThread[] tasks = new TaskThread[numberOfTasks];
 		
 		for(int i = 0; i < numberOfTasks; i++){
-			tasks.add(new TaskThread(i));
-			executorService.execute(tasks.get(i));
+			tasks[i] = new TaskThread(i);
+			executorService.execute(tasks[i]);
 		}
 		
 		executorService.shutdown();
