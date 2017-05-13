f1e1f68ab0a183576cedc84c1619a092689e4518
Avinash Raj <seriouswithavi@gmail.com>
2017-05-13 23:19:54 +0530

    Custom exception example

---
+++ Learn/src/learn/customeException/NameNotFoundException.java
@@ -0,0 +1,14 @@
+package learn.customeException;
+
+@SuppressWarnings("serial")
+public class NameNotFoundException extends Exception{
+
+	public NameNotFoundException(String message){
+		super(message);
+	}
+	
+	
+	public NameNotFoundException(String message, Throwable cause){
+		super(message, cause);
+	}
+}
