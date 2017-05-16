bd7723d4735bc45170b7baa7d84b61934a156597
Avinash Raj <seriouswithavi@gmail.com>
2017-05-17 03:23:31 +0530

    added comments

--- Learn/src/learn/generics/boundedType/GenericsNumber.java
+++ Learn/src/learn/generics/boundedType/GenericsNumber.java
@@ -1,14 +1,12 @@
 package learn.generics.boundedType;
-
-/*
- * A Generics example with bounded type
- * */
 /**
- * T must be either a Number, or a class derived from Number.
+ * A Generics example with bounded type
+ * Super class is upper bound
+ * T must be either a superclass(Number), or a class derived from superclass(Number) like Integer, Double.
  * */
 public class GenericsNumber<T extends Number> {
 
-	T[] number; //array of Number or sub class.
+	T[] number; //array of Number or sub classes.
 	
 	public GenericsNumber(T[] number) {
 		this.number = number;
@@ -22,6 +20,7 @@ public class GenericsNumber<T extends Number> {
 	double sum(){
 		double sum = 0.0;
 		for(int i = 0; i < number.length; i++){
+			//Number class define the doubleValue() method and this method is available to all numeric wrapper classes 
 			sum += number[i].doubleValue();
 		}
 		return sum;
