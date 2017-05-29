f017c0adc03b0524b84ef0ca4ef67f00a2534a2e
Avinash Raj <seriouswithavi@gmail.com>
2017-05-30 03:12:57 +0530

    ProcessBuilder demo

---
+++ Learn/src/learn/langPackage/ProcessBuilderDemo.java
@@ -0,0 +1,17 @@
+package learn.langPackage;
+
+import java.io.IOException;
+
+public class ProcessBuilderDemo {
+	
+	public static void main(String[] args) {
+		
+		try {
+			ProcessBuilder process = new ProcessBuilder("notepad.exe","testfile");
+			process.start();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
