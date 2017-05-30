dde2c6858cf9a8b08850aca07e4e66826629353e
Avinash Raj <seriouswithavi@gmail.com>
2017-05-31 03:06:12 +0530

    System class demo(Environment info)

---
+++ Learn/src/learn/langPackage/EnvironmentDemo.java
@@ -0,0 +1,14 @@
+package learn.langPackage;
+
+public class EnvironmentDemo {
+	
+	public static void main(String[] args) {
+		System.out.println(System.getProperty("os.name"));
+		System.out.println(System.getProperty("os.version"));
+		System.out.println(System.getProperty("user.dir"));
+		System.out.println(System.getProperty("java.version"));
+		System.out.println(System.getProperty("java.home"));
+		
+	}
+
+}
