7b230d70ac98e33053c378ef5719ad57a6875f2a
Avinash Raj <seriouswithavi@gmail.com>
2017-07-04 12:11:48 +0530

    NavigableSet inital commit

---
+++ Learn/src/learn/collection/NavigableSetDemo.java
@@ -0,0 +1,13 @@
+package learn.collection;
+
+import java.util.NavigableSet;
+import java.util.TreeSet;
+
+public class NavigableSetDemo {
+	
+	public static void main(String[] args) {
+		NavigableSet<String> navigableSet = new TreeSet<>();
+	}
+	
+
+}
