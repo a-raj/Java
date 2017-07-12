e09344a02b7073efc4eea564e0908faccb282379
avinanash <avinashraj2711@gmail.com>
2017-07-13 02:23:26 +0530

    Lambdas demo with threads

---
+++ Learn/src/java8/lambdas/LambdasDemo.java
@@ -0,0 +1,34 @@
+package java8.lambdas;
+
+
+public class LambdasDemo {
+	
+	public static void main(String[] args) {
+		Thread thread = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				System.out.println("Another thread");
+				
+			}
+		});
+		
+		thread.start();
+		
+		System.out.println("Inside main");
+		
+		
+		/*
+		 * With Lambda you required only function parameter and and body no name and return type
+		 * Compare it with above thread where we were using anonymous class
+		 * Here name became anonymous and return type inferred
+		 * 
+		 * */
+		
+		Thread thread2 = new Thread(() -> System.out.println("Lambda thread"));
+		thread2.start();
+	}
+	
+	
+	
+}
