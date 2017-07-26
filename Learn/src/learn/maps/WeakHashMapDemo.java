fd4e6fb4d1b898bcd7b9fd0eaf4986fb1d38b659
a-raj <seriouswithavi@gmail.com>
2017-07-26 11:40:59 +0530

    WeakHashMap and HashMap

---
+++ Learn/src/learn/maps/WeakHashMapDemo.java
@@ -0,0 +1,31 @@
+package learn.maps;
+
+import java.util.WeakHashMap;
+
+public class WeakHashMapDemo {
+
+	public static void main(String[] args) {
+		WeakHashMap<Data, String> weakHashMap = new WeakHashMap<>();
+		Data data = new Data(12);
+		weakHashMap.put(data, "Some String");
+		
+		data = null;
+		
+		for(int i = 1; i < 100000; i++) {
+			if(!weakHashMap.isEmpty()) {
+				System.out.println("At iteration "+ i +  " the map still hold the reference on data object");
+			}
+			else {
+				System.out.println("data  Object has been finally garbage collected at iteration "+ i +" hence the map is now empty");
+				break;
+			}
+		}
+	}
+	
+	static class Data{
+		int someNumber;
+		Data(int someNumber){
+			this.someNumber = someNumber;
+		}
+	}
+}
