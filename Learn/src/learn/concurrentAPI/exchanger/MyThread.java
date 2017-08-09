bb6bf46ed211f5448ee9bdb3db32217dae0c5ca4
a-raj <seriouswithavi@gmail.com>
2017-08-09 11:10:44 +0530

    Exchanger demo

---
+++ Learn/src/learn/concurrentAPI/exchanger/MyThread.java
@@ -0,0 +1,28 @@
+package learn.concurrentAPI.exchanger;
+
+import java.util.concurrent.Exchanger;
+
+public class MyThread extends Thread{
+	
+	Exchanger<String> exchanger;
+	String message;
+	
+	MyThread(Exchanger<String> exchanger, String message) {
+	      this.exchanger = exchanger;
+	      this.message = message;
+	}
+	
+	public void run() {
+		try {
+			System.out.println(this.getName() + " message: " + message);
+			
+			//Exchange message with the other thread 
+			message = exchanger.exchange(message);
+			Thread.sleep(1000);
+			System.out.println(this.getName() + " message: " + message);
+			
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
