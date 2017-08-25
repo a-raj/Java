257ab6198000d1ab755283cc6f5cb02956624f9c
a-raj <seriouswithavi@gmail.com>
2017-08-25 11:48:49 +0530

    Callable and Future

---
+++ Learn/src/learn/concurrentAPI/callable/CallableDemo.java
@@ -0,0 +1,28 @@
+package learn.concurrentAPI.callable;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class CallableDemo {
+	
+	public static void main(String[] args) {
+		
+		ExecutorService executorService = Executors.newFixedThreadPool(2);
+		
+		System.out.println("Starting");
+		Future<Integer> sumFuture = executorService.submit(new Sum(100));
+		Future<Integer> factorialFuture = executorService.submit(new Factorial(10));
+		
+		try {
+			System.out.println(sumFuture.get());
+			System.out.println(factorialFuture.get());
+		} catch (InterruptedException | ExecutionException e) {
+			e.printStackTrace();
+		}
+		executorService.shutdown();
+		System.out.println("Done");
+	}
+
+}
