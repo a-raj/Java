b530ce0af2cbb696ff098bf0514181c85faf09c8
a-raj <seriouswithavi@gmail.com>
2017-12-25 01:27:06 +0530

    Print Odd Even with 2 diff thread problem

---
+++ Learn/src/learn/concurrentAPI/synchronizers/phaser/PhaserEvenOdd.java
@@ -0,0 +1,34 @@
+package learn.concurrentAPI.synchronizers.phaser;
+
+import java.util.concurrent.Phaser;
+
+public class PhaserEvenOdd {
+
+    private static Phaser phaser = new Phaser(2);
+
+
+    public static void main(String[] args) {
+
+        Thread thread1 = new Thread(PhaserEvenOdd::runEven);
+        Thread thread2 = new Thread(PhaserEvenOdd::runOdd);
+        thread1.start();
+        thread2.start();
+    }
+
+    private static void runEven() {
+        for (int i = 2; i <= 100; i += 2) {
+            phaser.arriveAndAwaitAdvance();
+            System.out.println(i);
+            phaser.arriveAndAwaitAdvance();
+        }
+
+    }
+
+    private static void runOdd() {
+        for (int i = 1; i <= 100; i += 2) {
+            System.out.println(i);
+            phaser.arriveAndAwaitAdvance();
+            phaser.arriveAndAwaitAdvance();
+        }
+    }
+}
